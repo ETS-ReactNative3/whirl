647e10d1af1267ad6e1b7fd37b67e21e6c89b980
C2P1 <c2p1@users.noreply.github.com>
2018-06-18 21:27:17 +0100

    hide isMounted() warnings from node_modules

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 // redux
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './src/reducers'
-import thunk from 'redux-thunk'
-const store = createStore(rootReducer, applyMiddleware(thunk))
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './src/reducers';
+import thunk from 'redux-thunk';
+const store = createStore(rootReducer, applyMiddleware(thunk));
 // Amplify
-import config from './src/aws-exports'
-import Amplify from 'aws-amplify'
+import config from './src/aws-exports';
+import Amplify from 'aws-amplify';
+
+// hide isMounted() warnings due to them being in node_modules
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated']);
+
 Amplify.configure(config);
 // App
 const ReduxApp = () => (
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
 AppRegistry.registerComponent('Whirl', () => ReduxApp);
