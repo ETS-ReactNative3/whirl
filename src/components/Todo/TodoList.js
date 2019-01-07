fe4eeeaaaacdde16818707fb34e3b9ec8d4c2675
C2P1 <c2p1@users.noreply.github.com>
2019-01-07 12:46:16 +0000

    Implemented FlatList to replace the deprecated ListView.

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ListView } from 'react-native';
+import { View, FlatList } from 'react-native';
 import TodoItem from './TodoItem';
 
 class todoList extends Component {
@@ -9,16 +9,23 @@ class todoList extends Component {
 
   async componentDidMount() {
     this.setState({
-      apiResponse: this.props.apiResponse
-    });
-    var ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-    this.setState({
-      dataSource: ds.cloneWithRows(this.props.apiResponse),
       isLoading: !this.state.isLoading
     });
   }
+
+  _renderItem = (item, index) => (
+    <TodoItem
+      item={item}
+      todo={item.Content}
+      user={item.User}
+      strikethrough={item.Completed}
+      onDeletePressed={() =>
+        this.props.onDeletePressed(item.Date, item.User, index)
+      }
+    />
+  );
+
+  _keyExtractor = (item, index) => item.Date;
 
   render() {
     if (this.state.isLoading) {
@@ -26,45 +33,13 @@ class todoList extends Component {
     }
 
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        enableEmptySections={true}
-        renderRow={(rowData, sectionId, rowId) =>
-          rowId !== this.props.apiResponse.length - 1 ? (
-            <TodoItem
-              item={rowData}
-              todo={rowData.Content}
-              user={rowData.User}
-              strikethrough={rowData.Completed}
-              onDeletePressed={() =>
-                this.props.onDeletePressed(rowData.Date, rowData.User, rowId)
-              }
-            />
-          ) : (
-            <TodoItem
-              item={rowData}
-              todo={rowData.Content}
-              user={rowData.User}
-              strikethrough={rowData.Completed}
-              onDeletePressed={() =>
-                this.props.onDeletePressed(rowData.Date, rowData.User, rowId)
-              }
-              padding={true}
-            />
-          )
-        }
+      <FlatList
+        data={this.props.apiResponse}
+        keyExtractor={this._keyExtractor}
+        renderItem={({ item, index }) => this._renderItem(item, index)}
       />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  rowViewContainer: {
-    fontSize: 20,
-    paddingRight: 10,
-    paddingTop: 10,
-    paddingBottom: 10
-  }
-});
 
 export default todoList;
